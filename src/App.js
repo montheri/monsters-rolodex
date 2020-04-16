01df568a6bd162e55c4af7eb1aaee24d838e7cef
Monther Idkaidek <monti@midkadek.TNBank.ps>
2020-04-16 13:28:21 +0300

    adding search monster features

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField : ''
+
     }
   }
 
@@ -18,11 +20,17 @@ class App extends Component {
     .then(users => this.setState({monsters: users}))
     }
 
-
   render() {
+    const {monsters, searchField} = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
   return (
     <div className="App">
-      <CardList monsters = {this.state.monsters}/>
+      <input 
+        type='search'
+        placeholder = 'search monster'
+        onChange={e => this.setState({searchField: e.target.value})}
+      />
+      <CardList monsters = {filteredMonsters}/>
     </div>
   );
   }
