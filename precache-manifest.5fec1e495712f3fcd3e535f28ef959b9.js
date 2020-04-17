82f9359e091ae7b9f5bb4e1e1e8e9a47497a1eae
Monther Idkaidek <montheri@msn.com>
2020-04-17 16:41:20 +0300

    Updates

---
+++ precache-manifest.5fec1e495712f3fcd3e535f28ef959b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d677f2a6a700b9f149b771ced58ef80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8aa996f0a12057713c49",
+    "url": "/monsters-rolodex/static/css/main.7f69aec2.chunk.css"
+  },
+  {
+    "revision": "83708b8d3de79eae6b22",
+    "url": "/monsters-rolodex/static/js/2.833f470a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.833f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa996f0a12057713c49",
+    "url": "/monsters-rolodex/static/js/main.65b0cf31.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
